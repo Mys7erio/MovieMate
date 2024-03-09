2c2b6d48f95ebdf133584bc9f9290cf1518f5808
Shakir <shakirali.sacube@gmail.com>
2024-03-10 02:39:09 +0530

    Added test Navigation to login page and icon for app

--- app/src/main/java/com/example/moviemate/HomeFragment.kt
+++ app/src/main/java/com/example/moviemate/HomeFragment.kt
@@ -1,19 +1,34 @@
 package com.example.moviemate
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class HomeFragment : Fragment() {
 
+    private lateinit var loginBtn: Button
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Access the button from the inflated layout
+        val loginBtn = view.findViewById<Button>(R.id.loginBtnId)
+
+        // Set click listener for the button
+        loginBtn.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 }
