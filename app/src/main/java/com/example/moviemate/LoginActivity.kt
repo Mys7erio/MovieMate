a5e481e465b7faf2108b62ddc8d4f39f4c67ecac
Shakir <shakirali.sacube@gmail.com>
2024-03-10 15:55:39 +0530

    Added automatic navigation to Login page if the user is not logged in

--- app/src/main/java/com/example/moviemate/LoginActivity.kt
+++ app/src/main/java/com/example/moviemate/LoginActivity.kt
@@ -2,10 +2,27 @@ package com.example.moviemate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnSignIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnSignIn = findViewById(R.id.btnSignIn)
+
+        btnSignIn.setOnClickListener {
+            Toast.makeText(applicationContext, etUsername.text.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, etPassword.text.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/moviemate/MainActivity.kt
+++ app/src/main/java/com/example/moviemate/MainActivity.kt
@@ -2,50 +2,70 @@ package com.example.moviemate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import android.widget.Toast
+import android.view.MenuItem
+import androidx.core.content.ContentProviderCompat
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationBar: BottomNavigationView
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set initial fragment
         replaceFragment(HomeFragment())
 
+        // Initialize firebase auth
+        auth = FirebaseAuth.getInstance()
+//        Toast.makeText(applicationContext, auth.currentUser.toString(), Toast.LENGTH_SHORT).show()
+
         bottomNavigationBar = findViewById(R.id.bottomNavView)
-        bottomNavigationBar.setOnItemSelectedListener() { menuItem ->
-            when (menuItem.itemId) {
-                R.id.navitem_home -> {
-                    replaceFragment(HomeFragment())
-                    true
-                }
-
-                R.id.navitem_profile -> {
-                    replaceFragment(ProfileFragment())
-                    true
-                }
-
-                R.id.navitem_watchlist -> {
-                    replaceFragment(WatchlistFragment())
-                    true
-                }
-
-                R.id.navitem_watched -> {
-                    replaceFragment(WatchedFragment())
-                    true
-                }
-
-                else -> {
-                    Toast.makeText(applicationContext, "Invalid Option Selected", Toast.LENGTH_LONG).show()
-                    false
-                }
-            }
+        bottomNavigationBar.setOnItemSelectedListener { menuItem ->
+            handleNavigation(menuItem)
         }
 
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "Please Login...", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun handleNavigation(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.navitem_home -> {
+                replaceFragment(HomeFragment())
+                return true
+            }
+
+            R.id.navitem_profile -> {
+                replaceFragment(ProfileFragment())
+                return true
+            }
+
+            R.id.navitem_watchlist -> {
+                replaceFragment(WatchlistFragment())
+                return true
+            }
+
+            R.id.navitem_watched -> {
+                replaceFragment(WatchedFragment())
+                return true
+            }
+
+            else -> {
+                Toast.makeText(applicationContext, "Invalid Option Selected", Toast.LENGTH_LONG)
+                    .show()
+                return false
+            }
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
